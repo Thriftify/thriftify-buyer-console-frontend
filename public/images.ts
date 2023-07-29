69ef811093d9f8676e9f3548a4354815115cb9b0
Simlex <gettosimlex@gmail.com>
2023-07-29 10:16:44 +0100

    Updated images

--- public/images.ts
+++ public/images.ts
@@ -5,6 +5,11 @@ import loginImg2 from "./images/loginImg2.jpg";
 import loginImg3 from "./images/loginImg3.jpg";
 import loginImg4 from "./images/loginImg4.jpg";
 import wordmark_white from "./images/wordmark_white.png";
+import heroImg from "./images/heroImg.jpg";
+import car_img from "./images/car_img.jpg";
+import memoji_1 from "./images/memoji_1.png";
+import memoji_2 from "./images/memoji_2.png";
+import memoji_3 from "./images/memoji_3.png";
 
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
@@ -13,5 +18,10 @@ export default {
   loginImg2,
   loginImg3,
   loginImg4,
-  wordmark_white
+  wordmark_white,
+  heroImg,
+  car_img,
+  memoji_1,
+  memoji_2,
+  memoji_3
 };
