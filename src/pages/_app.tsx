0c207e7328155fad84c27f9eccd6835ec427bad3
Simlex <gettosimlex@gmail.com>
2023-07-29 10:17:45 +0100

    Added page loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,32 @@
 import Layout from '@/components/Layout'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Image from 'next/image';
+import { useEffect, useState } from 'react'
+import images from '../../public/images';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (<Layout>
-        <Component {...pageProps} />
-    </Layout>)
+    const [loaderIsVisible, setLoaderIsVisible] = useState(true);
+
+    // setTimeout(() => {
+    //     setLoaderIsVisible(false);
+    // }, 1000);
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            setLoaderIsVisible(false);
+        }
+    }, [])
+
+    return (
+        <>
+            {!loaderIsVisible && <Layout>
+                <Component {...pageProps} />
+            </Layout>}
+            {loaderIsVisible && <div className="splashScreen">
+                <div className="image">
+                    <Image src={images.blueLogo} alt='logo' />
+                </div>
+            </div>}
+        </>
+    )
 }
